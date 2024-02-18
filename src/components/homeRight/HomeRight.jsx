0ffec292d2ab7b1a315404253d246d13f616957e
myjhye <t01087633799@gmail.com>
2024-02-18 20:14:25 +0900

    HomeRight, StoryCircle (초안)

---
+++ src/components/homeRight/HomeRight.jsx
@@ -0,0 +1,7 @@
+export default function HomeRight() {
+    return (
+        <div className="border">
+            home right!!
+        </div>
+    )
+}
---
+++ src/components/story/StoryCircle.jsx
@@ -0,0 +1,8 @@
+export default function StoryCircle() {
+    return (
+        <div className="cursor-pointer flex flex-col items-center">
+            <img className="w-16 h-16 rounded-full" src="https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202303/02/bemypet/20230302170052142ncnl.jpg" alt="" />
+            <p>username</p>
+        </div>
+    )
+}
